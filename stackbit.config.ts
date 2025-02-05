ae50cd04438baf21ad692f29801fc25c465cb9d0
Zouavee <hakim.awd29@gmail.com>
2025-02-05 14:29:00 +0100

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,9 +1,15 @@
 import { defineStackbitConfig } from '@stackbit/types';
 
 export default defineStackbitConfig({
-    "stackbitVersion": "~0.6.0",
-    "nodeVersion": "18",
-    "ssgName": "custom",
-    "contentSources": [],
+    "stackbitVersion": "~0.6.0",        // Cette version est correcte
+    "nodeVersion": "18",                // Vérifie si c'est bien la version compatible
+    "ssgName": "custom",                // Assure-toi que tu utilises bien la bonne configuration pour ton générateur de site statique (SSG)
+    "contentSources": [
+        {
+            "name": "github",
+            "url": "https://github.com/ton-utilisateur/ton-depot", // Remplace par l'URL de ton dépôt GitHub
+            "branch": "main"  // Vérifie que tu travailles sur la bonne branche, ici "main"
+        }
+    ],
     "postInstallCommand": "npm i --no-save @stackbit/types"
-})
+});
